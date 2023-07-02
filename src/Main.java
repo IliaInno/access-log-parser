7a5538ccd1dae86a7c549c5ded66a1ae71d2957d
Ilia Ka <kalinin499studing@gmail.com>
2023-07-02 22:49:30 +0300

    add exception handling and count lines

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -12,15 +12,49 @@ public class Main {
 
             boolean isDirectory = file.isDirectory();
 
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или путь является путём к папке");
+                continue;
+            }
+
             if (fileExists) {
                 System.out.println("Путь указан верно");
                 System.out.println("Это файл номер " + count);
                 count++;
-                continue;
-            }
 
-            if (!fileExists || isDirectory) {
-                System.out.println("Указанный файл не существует или путь является путём к папке");
+                if (!fileExists || isDirectory) {
+                    System.out.println("Указанный файл не существует или путь является путём к папке");
+                }
+
+                try {
+                    int minLength = Integer.MAX_VALUE;
+                    int maxLength = 0;
+                    int totalLinesCount = 0;
+
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        int currentLength = line.length();
+                        if (currentLength > 1024) {
+                            throw new RuntimeException("Длина строки более 1024 символов");
+                        }
+                        if (currentLength > maxLength) {
+                            maxLength = currentLength;
+                        }
+                        if (currentLength < minLength) {
+                            minLength = currentLength;
+                        }
+                        totalLinesCount++;
+                    }
+                    System.out.println("Общее количество строк в файле: " + totalLinesCount);
+                    System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                    System.out.println("Длина самой короткой строки в файле: " + minLength);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
